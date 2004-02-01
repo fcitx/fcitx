0498a49af10ac7ec08e1fdcdbf3a6fc41fa429c1
xyb <devnull@localhost>
2004-02-01 20:19:44 +0000

    add declaring

--- autogen.sh
+++ autogen.sh
@@ -18,3 +18,25 @@ else
   ./configure $* || exit 4
   make || exit 5
 fi
+
+echo
+echo 'Build and Install on system:'
+echo '  ./configure && make install   # run as root'
+
+echo 'Build and Install on user home:'
+echo '  ./configure --prefix="$HOME" && make install'
+
+echo 'Build without xft:'
+echo '  ./configure --enable-xft=no && make'
+
+echo
+echo 'Build RPM Package like these:'
+echo '  ./configure && make dist'
+echo '  rpmbuild -ts fcitx-*.tar.gz   # build source package'
+echo '  rpmbuild -tb fcitx-*.tar.gz   # build binary package'
+
+echo
+echo 'Build DEBIAN Package like these:'
+echo '  ./configure'
+echo '  vim ./debian/changelog        # add change log'
+echo '  fakeroot dpkg-buildpackage'
