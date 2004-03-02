069f6e910f5afed56da843eb2f78333993ea5198
xyb <devnull@localhost>
2004-03-02 21:55:46 +0000

    update

--- fcitx.spec
+++ fcitx.spec
@@ -1,5 +1,5 @@
 %define name		fcitx
-%define version		2.0.1
+%define version		2.0.2
 %define release		3
 
 %define prefix		/usr
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define FCITX_VERSION		"2.0.1"
+#define FCITX_VERSION		"2.0.2"
 
 #endif
