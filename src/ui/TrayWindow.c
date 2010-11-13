3cf1e0252cf619251b77fff89a1efda708a063a7
wengxt@gmail.com <wengxt@gmail.com>
2010-11-13 11:13:21 +0000

    bug fix of tray icon, update tray icon color to make it more clear with dark background

--- src/ui/TrayWindow.c
+++ src/ui/TrayWindow.c
@@ -46,7 +46,7 @@ Bool CreateTrayWindow() {
     XTextProperty tp;
     char   strWindowName[]="Fcitx Tray Window";
     if ( !fc.bUseTrayIcon )
-            return False;
+        return False;
 
     InitTray(dpy, &tray);
 
@@ -60,14 +60,14 @@ Bool CreateTrayWindow() {
         wsa.background_pixel = 0;
         wsa.border_pixel = 0;
         tray.window = XCreateWindow(dpy, p, -1, -1, 1, 1,
-                0, vi->depth, InputOutput, vi->visual,
-                CWBackPixmap|CWBackPixel|CWBorderPixel|CWColormap, &wsa);
+                                    0, vi->depth, InputOutput, vi->visual,
+                                    CWBackPixmap|CWBackPixel|CWBorderPixel|CWColormap, &wsa);
     }
     else {
         tray.window = XCreateSimpleWindow (dpy, DefaultRootWindow(dpy), \
-            -1, -1, 1, 1, 0, \
-            BlackPixel (dpy, DefaultScreen (dpy)), \
-            WhitePixel (dpy, DefaultScreen (dpy)));
+                                           -1, -1, 1, 1, 0, \
+                                           BlackPixel (dpy, DefaultScreen (dpy)), \
+                                           WhitePixel (dpy, DefaultScreen (dpy)));
         XSetWindowBackgroundPixmap(dpy, tray.window, ParentRelative);
     }
     if (tray.window == (Window) NULL)
@@ -86,7 +86,7 @@ Bool CreateTrayWindow() {
     tp.format = 16;
     tp.nitems = strlen(strWindowName);
     XSetWMName (dpy, tray.window, &tp);
-   
+
     if (vi && vi->visual)
         tray.cs = cairo_xlib_surface_create(dpy, tray.window, tray.visual.visual, 200, 200);
     else
@@ -96,8 +96,8 @@ Bool CreateTrayWindow() {
     }
 
     XSelectInput (dpy, tray.window, ExposureMask | KeyPressMask | \
-            ButtonPressMask | ButtonReleaseMask | StructureNotifyMask \
-            | EnterWindowMask | PointerMotionMask | LeaveWindowMask | VisibilityChangeMask);
+                  ButtonPressMask | ButtonReleaseMask | StructureNotifyMask \
+                  | EnterWindowMask | PointerMotionMask | LeaveWindowMask | VisibilityChangeMask);
     return True;
 }
 
@@ -112,14 +112,14 @@ void DestroyTrayWindow()
 
 void DrawTrayWindow(int f_state, int x, int y, int w, int h) {
     if ( !fc.bUseTrayIcon )
-            return;
+        return;
 
-	cairo_t *c;
-	cairo_surface_t *png_surface ;
+    cairo_t *c;
+    cairo_surface_t *png_surface ;
     FcitxImage* skinImg;
     if (!tray.bTrayMapped) {
         if (!TrayFindDock(dpy, &tray))
-			return;
+            return;
     }
 
     /* 画png */
@@ -149,15 +149,15 @@ void DrawTrayWindow(int f_state, int x, int y, int w, int h) {
         XClearArea (dpy, tray.window, x, y, w, h, False);
     }
 
-    if( strlen(skinImg->img_name) != 0 && strcmp( skinImg->img_name ,"NONE.img") != 0)
+    if ( strlen(skinImg->img_name) != 0 && strcmp( skinImg->img_name ,"NONE.img") != 0)
     {
         cairo_scale(c, ((double) tray.size) / skinImg->height, ((double) tray.size) / skinImg->width);
         cairo_set_source_surface(c, png_surface, x , y );
         cairo_set_operator(c, CAIRO_OPERATOR_OVER);
         cairo_paint_with_alpha(c,1);
     }
-	
-	cairo_destroy(c);
+
+    cairo_destroy(c);
 
 }
 
@@ -166,61 +166,59 @@ void TrayEventHandler(XEvent* event)
     if (!fc.bUseTrayIcon)
         return;
     switch (event->type) {
-		case ClientMessage:
-			if (event->xclient.message_type == tray.atoms[ATOM_MANAGER]
-			 && event->xclient.data.l[1] == tray.atoms[ATOM_SELECTION])
-			{
-                if (tray.window == None)
-                    CreateTrayWindow();
-                TrayFindDock(dpy, &tray);
-			}
-			break;
+    case ClientMessage:
+        if (event->xclient.message_type == tray.atoms[ATOM_MANAGER]
+                && event->xclient.data.l[1] == tray.atoms[ATOM_SELECTION])
+        {
+            if (tray.window == None)
+                CreateTrayWindow();
+            TrayFindDock(dpy, &tray);
+        }
+        break;
 
-		case Expose:
-			if (event->xexpose.window == tray.window) {
-                if (GetCurrentState() == IS_CHN)
-                    DrawTrayWindow (ACTIVE_ICON, 0, 0, tray.size, tray.size);
-                else
-                    DrawTrayWindow (INACTIVE_ICON, 0, 0, tray.size, tray.size);
-			}
-			break;
-        case ConfigureNotify:
-            if (tray.window == event->xconfigure.window)
+    case Expose:
+        if (event->xexpose.window == tray.window) {
+            if (GetCurrentState() == IS_CHN)
+                DrawTrayWindow (ACTIVE_ICON, 0, 0, tray.size, tray.size);
+            else
+                DrawTrayWindow (INACTIVE_ICON, 0, 0, tray.size, tray.size);
+        }
+        break;
+    case ConfigureNotify:
+        if (tray.window == event->xconfigure.window)
+        {
+            int size = event->xconfigure.height;
+            if (size != tray.size)
             {
-                int size = event->xconfigure.height;
-                if (size != tray.size)
-                {
-                    tray.size = size;
-                    XSizeHints size_hints;
-                    size_hints.flags = PWinGravity | PBaseSize;
-                    size_hints.base_width = tray.size;
-                    size_hints.base_height = tray.size;
-                    XSetWMNormalHints(dpy, tray.window, &size_hints);
-                }
-
-                if (GetCurrentState() == IS_CHN)
-                    DrawTrayWindow (ACTIVE_ICON, 0, 0, tray.size, tray.size);
-                else
-                    DrawTrayWindow (INACTIVE_ICON, 0, 0, tray.size, tray.size);
-                break;
+                tray.size = size;
+                XSizeHints size_hints;
+                size_hints.flags = PWinGravity | PBaseSize;
+                size_hints.base_width = tray.size;
+                size_hints.base_height = tray.size;
+                XSetWMNormalHints(dpy, tray.window, &size_hints);
             }
 
+            if (GetCurrentState() == IS_CHN)
+                DrawTrayWindow (ACTIVE_ICON, 0, 0, tray.size, tray.size);
+            else
+                DrawTrayWindow (INACTIVE_ICON, 0, 0, tray.size, tray.size);
+        }
+        break;
 
-		case DestroyNotify:
+
+    case DestroyNotify:
+        tray.bTrayMapped = False;
+        DestroyTrayWindow();
+        break;
+
+    case ReparentNotify:
+        if (event->xreparent.parent == DefaultRootWindow(dpy) && event->xreparent.window == tray.window)
+        {
             tray.bTrayMapped = False;
             DestroyTrayWindow();
-			break;
-
-        case ReparentNotify:
-            {
-                if (event->xreparent.parent == DefaultRootWindow(dpy) && event->xreparent.window == tray.window)
-                {
-                    tray.bTrayMapped = False;
-                    DestroyTrayWindow();
-                }
-            }
-            break;
-	}
+        }
+        break;
+    }
 }
 
 #endif
