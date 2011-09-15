3ced9818039ac36880a84682b0b929cd9e8850d3
csslayer <wengxt@gmail.com>
2011-09-15 23:57:49 +0800

    add missing file

---
+++ src/lib/fcitx/fcitx-internal.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *   Copyright (C) 2011~2011 by CSSlayer                                   *
+ *   wengxt@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef _FCITX_INTERNAL_H_
+#define _FCITX_INTERNAL_H_
+
+/**
+ * @file fcitx-internal.h
+ * @author CS Slayer <wengxt@gmail.com>
+ * @brief some misc definition for Fcitx
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FCITX_GETTER_REF(type, property, name, return_type) \
+FCITX_EXPORT_API \
+return_type* type##Get##property(type* object) \
+{ \
+    return &object->name; \
+}
+
+#define FCITX_GETTER_VALUE(type, property, name, return_type) \
+FCITX_EXPORT_API \
+return_type type##Get##property(type* object) \
+{ \
+    return object->name; \
+}
+
+#define FCITX_SETTER(type, property, name, return_type) \
+FCITX_EXPORT_API \
+void type##Set##property(type* object, return_type value) \
+{ \
+    object->name = value; \
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif/*_FCITX_H_*/
+
+// kate: indent-mode cstyle; space-indent on; indent-width 0;
---
+++ src/lib/fcitx/instance-internal.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 2011~2011 by CSSlayer                                   *
+ *   wengxt@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/**
+ * @file   instance-internal.h
+ *
+ */
+
+
+#ifndef _FCITX_INSTANCE_INTERNAL_H_
+#define _FCITX_INSTANCE_INTERNAL_H_
+
+#include "fcitx-utils/utarray.h"
+#include "configfile.h"
+#include "profile.h"
+
+struct _FcitxInstance {
+    pthread_mutex_t fcitxMutex;
+    UT_array uistats;
+    UT_array uimenus;
+    FcitxAddon* ui;
+    FcitxInputState* input;
+    boolean bMutexInited;
+    FcitxUIMenu imMenu;
+
+    /* Fcitx is not good at multi process, so put a readonlyMode in it */
+    boolean readonlyMode;
+
+    /* config file */
+    FcitxConfig* config;
+    FcitxProfile* profile;
+    UT_array addons;
+    UT_array imeclasses;
+    UT_array imes;
+    UT_array frontends;
+    UT_array eventmodules;
+
+    struct _FcitxInputContext *CurrentIC;
+    struct _FcitxInputContext *ic_list;
+    struct _FcitxInputContext *free_list;
+    sem_t* sem;
+    pthread_t pid;
+    fd_set rfds, wfds, efds;
+    int maxfd;
+    char* uiname;
+
+    struct _HookStack* hookPreInputFilter;
+    struct _HookStack* hookPostInputFilter;
+    struct _HookStack* hookOutputFilter;
+    struct _HookStack* hookHotkeyFilter;
+    struct _HookStack* hookResetInputHook;
+    struct _HookStack* hookTriggerOnHook;
+    struct _HookStack* hookTriggerOffHook;
+    struct _HookStack* hookInputFocusHook;
+    struct _HookStack* hookInputUnFocusHook;
+    struct _HookStack* hookUpdateCandidateWordHook;
+
+    FcitxUIFlag uiflag;
+
+    IME_STATE globalState;
+
+    time_t totaltime;
+    time_t timeStart;
+    int iHZInputed;
+
+    /* gives more padding, since we want to break abi */
+    int padding[64];
+};
+
+#endif
