836f2eefa6b7d5f52db1dd8bb3905ef938342bf4
Weng Xuetian <wengxt@gmail.com>
2011-10-06 01:24:52 +0800

    don't filter all release event

--- src/lib/fcitx/ime.c
+++ src/lib/fcitx/ime.c
@@ -414,11 +414,13 @@ INPUT_RETURN_VALUE ProcessKey(
         }
     }
 
+#if 0
     /* Added by hubert_star AT forum.ubuntu.com.cn */
     if (event == FCITX_RELEASE_KEY
             && IsHotKeySimple(sym, state)
             && retVal == IRV_TO_PROCESS)
         return IRV_DO_NOTHING;
+#endif
 
     if (retVal == IRV_TO_PROCESS) {
         /* process key event for switch key */
