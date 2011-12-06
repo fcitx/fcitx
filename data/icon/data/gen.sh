bfc956df2d6aa4fe3fd5721dff113553b3a89e94
Weng Xuetian <wengxt@gmail.com>
2011-12-06 15:06:40 +0800

    update icon

--- data/icon/data/gen.sh
+++ data/icon/data/gen.sh
@@ -8,6 +8,18 @@ do
     ff=`basename $f`
     for s in $size
     do
-        inkscape-0.48.2-1 -e /home/saber/Develop/fcitx/data/icon/${s}x${s}/status/${ff/.svg/.png} -w $s -h $s -b "000000" -y 0.0 /home/saber/Develop/fcitx/data/icon/$f
+        inkscape -e /home/saber/Develop/fcitx/data/icon/${s}x${s}/status/${ff/.svg/.png} -w $s -h $s -b "000000" -y 0.0 /home/saber/Develop/fcitx/data/icon/$f
     done
+done
+
+for f in fcitx-keyboard-16.svg fcitx-vk-active-16.svg fcitx-vk-inactive-16.svg
+do
+inkscape -e /home/saber/Develop/fcitx/data/icon/16x16/status/${f/-16.svg/.png} -w 16 -h 16 -b "000000" -y 0.0 /home/saber/Develop/fcitx/data/icon/data/$f
+done
+
+
+
+for f in fcitx-fullwidth-active.svg fcitx-fullwidth-inactive.svg fcitx-punc-active.svg fcitx-punc-inactive.svg
+do
+inkscape -e /home/saber/Develop/fcitx/data/icon/16x16/status/${f/.svg/.png} -w 16 -h 16 -b "000000" -y 0.0 /home/saber/Develop/fcitx/data/icon/scabable/status/$f
 done
