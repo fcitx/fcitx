65a618bf73323335a42df1e5ebd57429677892da
Weng Xuetian <wengxt@gmail.com>
2011-12-25 03:26:18 +0800

    add function for chttrans.

--- src/module/chttrans/chttrans.c
+++ src/module/chttrans/chttrans.c
@@ -19,23 +19,25 @@
  ***************************************************************************/
 
 #include "fcitx/fcitx.h"
+#include "config.h"
 
 #ifdef _ENABLE_OPENCC
 #include <opencc.h>
 #endif
 
 #include <libintl.h>
+#include <errno.h>
 
 #include "fcitx/module.h"
 #include "fcitx-utils/utf8.h"
 #include "fcitx-utils/uthash.h"
 #include "fcitx-config/xdg.h"
-#include <errno.h>
 #include "fcitx/hook.h"
 #include "fcitx/ui.h"
 #include "fcitx-utils/log.h"
 #include "fcitx/instance.h"
-#include <fcitx-utils/utils.h>
+#include "fcitx-utils/utils.h"
+#include "chttrans.h"
 
 #define TABLE_GBKS2T "gbks2t.tab"
 
@@ -58,6 +60,11 @@ typedef struct _FcitxChttrans {
     ChttransEngine engine;
     FcitxHotkey hkToggle[2];
     simple2trad_t* s2t_table;
+    simple2trad_t* t2s_table;
+#ifdef _ENABLE_OPENCC
+    opencc_t ods2t;
+    opencc_t odt2s;
+#endif
     FcitxInstance* owner;
 } FcitxChttrans;
 
@@ -65,12 +72,15 @@ static void* ChttransCreate(FcitxInstance* instance);
 static char* ChttransOutputFilter(void* arg, const char* strin);
 static void ReloadChttrans(void* arg);
 static char *ConvertGBKSimple2Tradition(FcitxChttrans* transState, const char* strHZ);
+static char *ConvertGBKTradition2Simple(FcitxChttrans* transState, const char *strHZ);
 static boolean GetChttransEnabled();
 boolean LoadChttransConfig(FcitxChttrans* transState);
 static FcitxConfigFileDesc* GetChttransConfigDesc();
 static void SaveChttransConfig(FcitxChttrans* transState);
 static INPUT_RETURN_VALUE HotkeyToggleChttransState(void*);
 static void ToggleChttransState(void*);
+static void* ChttransS2T(void* arg, FcitxModuleFunctionArg args);
+static void* ChttransT2S(void* arg, FcitxModuleFunctionArg args);
 
 CONFIG_BINDING_BEGIN(FcitxChttrans)
 CONFIG_BINDING_REGISTER("TraditionalChinese", "TransEngine", engine)
@@ -93,6 +103,7 @@ int ABI_VERSION = FCITX_ABI_VERSION;
 void* ChttransCreate(FcitxInstance* instance)
 {
     FcitxChttrans* transState = fcitx_utils_malloc0(sizeof(FcitxChttrans));
+    FcitxAddon* transAddon = FcitxAddonsGetAddonByName(FcitxInstanceGetAddons(instance), FCITX_CHTTRANS_NAME);
     transState->owner = instance;
     if (!LoadChttransConfig(transState)) {
         free(transState);
@@ -112,6 +123,10 @@ void* ChttransCreate(FcitxInstance* instance)
     FcitxInstanceRegisterOutputFilter(instance, shk);
     FcitxInstanceRegisterCommitFilter(instance, shk);
     FcitxUIRegisterStatus(instance, transState, "chttrans", _("Traditional Chinese Translate"), _("Traditional Chinese Translate"), ToggleChttransState, GetChttransEnabled);
+    
+    AddFunction(transAddon, ChttransS2T);
+    AddFunction(transAddon, ChttransT2S);
+    
     return transState;
 }
 
@@ -138,10 +153,37 @@ boolean GetChttransEnabled(void* arg)
 char* ChttransOutputFilter(void* arg, const char *strin)
 {
     FcitxChttrans* transState = (FcitxChttrans*) arg;
-    if (transState->enabled)
-        return ConvertGBKSimple2Tradition(transState, strin);
-    else
-        return NULL;
+    if (transState->enabled) {
+        FcitxIM* im = FcitxInstanceGetCurrentIM(transState->owner);
+        if (im && (strcmp(im->langCode, "zh_HK") == 0 || strcmp(im->langCode, "zh_TW") == 0))
+            return NULL;
+        else
+            return ConvertGBKSimple2Tradition(transState, strin);
+    }
+    else {
+        FcitxIM* im = FcitxInstanceGetCurrentIM(transState->owner);
+        if (im && strcmp(im->langCode, "zh_CN") == 0)
+            return NULL;
+        else
+            return ConvertGBKTradition2Simple(transState, strin);
+    }
+    return NULL;
+}
+
+void* ChttransS2T(void* arg, FcitxModuleFunctionArg args)
+{
+    FcitxChttrans* transState = (FcitxChttrans*) arg;
+    const char* s = args.args[0];
+    
+    return ConvertGBKSimple2Tradition(transState, s);
+}
+
+void* ChttransT2S(void* arg, FcitxModuleFunctionArg args)
+{
+    FcitxChttrans* transState = (FcitxChttrans*) arg;
+    const char* s = args.args[0];
+    
+    return ConvertGBKTradition2Simple(transState, s);
 }
 
 /**
@@ -160,16 +202,15 @@ char *ConvertGBKSimple2Tradition(FcitxChttrans* transState, const char *strHZ)
     case ENGINE_OPENCC:
 #ifdef _ENABLE_OPENCC
         {
-            static opencc_t od = NULL;
-            if (od == NULL) {
-                od = opencc_open(OPENCC_DEFAULT_CONFIG_SIMP_TO_TRAD);
-                if (od == (opencc_t) - 1) {
+            if (transState->ods2t == NULL) {
+                transState->ods2t = opencc_open(OPENCC_DEFAULT_CONFIG_SIMP_TO_TRAD);
+                if (transState->ods2t == NULL) {
                     opencc_perror(_("OpenCC initialization error"));
                     return NULL;
                 }
             }
 
-            char * res = opencc_convert_utf8(od, strHZ, (size_t) - 1);
+            char * res = opencc_convert_utf8(transState->ods2t, strHZ, (size_t) - 1);
 
             if (res == (char *) - 1) {
                 opencc_perror(_("OpenCC error"));
@@ -231,6 +272,110 @@ char *ConvertGBKSimple2Tradition(FcitxChttrans* transState, const char *strHZ)
             if (s2t) {
                 strcat(ret, s2t->str);
                 ret_len += s2t->len;
+            } else {
+                strncat(ret, ps, chr_len);
+                ret_len += chr_len;
+            }
+
+            ps = nps;
+
+        }
+        ret[ret_len] = '\0';
+
+        return ret;
+    }
+    }
+    return NULL;
+}
+
+/**
+ * 该函数装载data/gbks2t.tab的简体转繁体的码表，
+ * 然后按码表将GBK字符转换成GBK繁体字符。
+ *
+ * WARNING： 该函数返回新分配内存字符串，请调用者
+ * 注意释放。
+ */
+char *ConvertGBKTradition2Simple(FcitxChttrans* transState, const char *strHZ)
+{
+    if (strHZ == NULL)
+        return NULL;
+
+    switch (transState->engine) {
+    case ENGINE_OPENCC:
+#ifdef _ENABLE_OPENCC
+        {
+            if (transState->odt2s == NULL) {
+                transState->odt2s = opencc_open(OPENCC_DEFAULT_CONFIG_TRAD_TO_SIMP);
+                if (transState->odt2s == NULL) {
+                    opencc_perror(_("OpenCC initialization error"));
+                    return NULL;
+                }
+            }
+
+            char * res = opencc_convert_utf8(transState->odt2s, strHZ, (size_t) - 1);
+
+            if (res == (char *) - 1) {
+                opencc_perror(_("OpenCC error"));
+                return NULL;
+            }
+
+            return res;
+        }
+#endif
+    case ENGINE_NATIVE: {
+        FILE           *fp;
+        char           *ret;
+        int             i, len, ret_len;
+        char           *strBuf = NULL;
+        size_t          bufLen = 0;
+        const char     *ps;
+
+        if (!transState->t2s_table) {
+            len = 0;
+
+            fp = FcitxXDGGetFileWithPrefix("data", TABLE_GBKS2T, "rb", NULL);
+            if (!fp) {
+                ret = (char *) malloc(sizeof(char) * (strlen(strHZ) + 1));
+                strcpy(ret, strHZ);
+                return ret;
+            }
+            while (getline(&strBuf, &bufLen, fp) != -1) {
+                simple2trad_t *t2s;
+                char *ps;
+                int wc;
+
+                ps = fcitx_utf8_get_char(strBuf, &wc);
+                t2s = (simple2trad_t*) malloc(sizeof(simple2trad_t));
+                
+                fcitx_utf8_get_char(ps, &wc);
+                t2s->wc = wc;
+                t2s->len = fcitx_utf8_char_len(strBuf);
+                strncpy(t2s->str, strBuf, t2s->len);
+                t2s->str[t2s->len] = '\0';
+
+                HASH_ADD_INT(transState->t2s_table, wc, t2s);
+            }
+            if (strBuf)
+                free(strBuf);
+        }
+
+        i = 0;
+        len = fcitx_utf8_strlen(strHZ);
+        ret_len = 0;
+        ret = (char *) fcitx_utils_malloc0(sizeof(char) * (UTF8_MAX_LENGTH * len + 1));
+        ps = strHZ;
+        ret[0] = '\0';
+        for (; i < len; ++i) {
+            int wc;
+            simple2trad_t *t2s = NULL;
+            int chr_len = fcitx_utf8_char_len(ps);
+            char *nps;
+            nps = fcitx_utf8_get_char(ps , &wc);
+            HASH_FIND_INT(transState->s2t_table, &wc, t2s);
+
+            if (t2s) {
+                strcat(ret, t2s->str);
+                ret_len += t2s->len;
             } else {
                 strncat(ret, ps, chr_len);
                 ret_len += chr_len;
---
+++ src/module/chttrans/chttrans.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *   Copyright (C) 2010~2010 by CSSlayer                                   *
+ *   wengxt@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
+#ifndef _FCITX_MODULE_CHTTRANS_H_
+#define _FCITX_MODULE_CHTTRANS_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FCITX_CHTTRANS_NAME "fcitx-chttrans"
+#define FCITX_CHTTRANS_S2T 0
+#define FCITX_CHTTRANS_S2T_RETURNTYPE char*
+#define FCITX_CHTTRANS_T2S 1
+#define FCITX_CHTTRANS_T2S_RETURNTYPE char*
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
