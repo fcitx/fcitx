0f3ecb14e735327ed41b4750b4d06d2ffd690936
Weng Xuetian <wengxt@gmail.com>
2012-02-02 00:41:59 +0800

    add some key needed by m17n

--- src/lib/fcitx-config/hotkey.h
+++ src/lib/fcitx-config/hotkey.h
@@ -2444,6 +2444,9 @@ extern "C"
         FcitxKeyState_MousePressed = 1 << 8,
         FcitxKeyState_HandledMask = 1 << 24,
         FcitxKeyState_IgnoredMask = 1 << 25,
+        FcitxKeyState_Super2    = 1 << 26,
+        FcitxKeyState_Hyper    = 1 << 27,
+        FcitxKeyState_Meta     = 1 << 28,
         FcitxKeyState_UsedMask = 0x5c001fff
     } FcitxKeyState;
 
