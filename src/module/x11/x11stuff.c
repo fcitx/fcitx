2216322cbc7cdba1c9feae5612edecfa4761e9cc
Weng Xuetian <wengxt@gmail.com>
2012-04-26 01:26:17 +0800

    Fixes Issue 556.

--- src/module/x11/x11stuff.c
+++ src/module/x11/x11stuff.c
@@ -18,6 +18,7 @@
  *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
  ***************************************************************************/
 
+#include "config.h"
 #include "fcitx/fcitx.h"
 
 #include <limits.h>
