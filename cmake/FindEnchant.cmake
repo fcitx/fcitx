f4254383cf1c086ec00e6317c6aa47f09a7111f2
Yichao Yu <yyc1992@gmail.com>
2012-08-13 12:09:28 -0400

    check api compatible of enchant and presage at compile time, add option to enable enchant and presage even if they are not found at compile time

--- cmake/FindEnchant.cmake
+++ cmake/FindEnchant.cmake
@@ -11,23 +11,60 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 if(ENCHANT_INCLUDE_DIR AND ENCHANT_LIBRARIES)
-    # Already in cache, be silent
-    set(ENCHANT_FIND_QUIETLY TRUE)
+  # Already in cache, be silent
+  set(ENCHANT_FIND_QUIETLY TRUE)
 endif(ENCHANT_INCLUDE_DIR AND ENCHANT_LIBRARIES)
 
 find_package(PkgConfig)
 pkg_check_modules(PC_ENCHANT enchant)
 
 find_path(ENCHANT_INCLUDE_DIR
-          NAMES enchant.h
-          HINTS ${PC_ENCHANT_INCLUDEDIR}
-          PATH_SUFFIXES "enchant")
+  NAMES enchant.h
+  HINTS ${PC_ENCHANT_INCLUDEDIR}
+  PATH_SUFFIXES "enchant")
 
 find_library(ENCHANT_LIBRARIES
-             NAMES enchant
-             HINTS ${PC_ENCHANT_LIBDIR})
+  NAMES enchant
+  HINTS ${PC_ENCHANT_LIBDIR})
+
+if(ENCHANT_INCLUDE_DIR AND ENCHANT_LIBRARIES)
+  check_c_compiler_flag("-Werror" ENCHANT_HAVE_WERROR)
+  set(CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
+  if(ICONV_HAVE_WERROR)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
+  endif(ICONV_HAVE_WERROR)
+  set(CMAKE_REQUIRED_INCLUDES "${ENCHANT_INCLUDE_DIR}")
+  set(CMAKE_REQUIRED_LIBRARIES "${ENCHANT_LIBRARIES}")
+  check_c_source_compiles("
+  #include <stdlib.h>
+  #include <stddef.h>
+  #include <string.h>
+  #include <enchant/enchant.h>
+  int main()
+  {
+      void *enchant;
+      void *dict;
+      char **suggestions = NULL;
+      size_t number;
+      enchant = enchant_broker_init();
+      enchant_broker_set_ordering(enchant, \"*\", \"aspell,myspell,ispell\");
+      dict = enchant_broker_request_dict(enchant, \"en\");
+      enchant_dict_add_to_personal(dict, \"Fcitx\", strlen(\"Fcitx\"));
+      suggestions = enchant_dict_suggest(dict, \"Fcitx\", strlen(\"Fcitx\"),
+                                         &number);
+      enchant_dict_free_string_list(dict, suggestions);
+      enchant_broker_free_dict(enchant, dict);
+      enchant_broker_free(enchant);
+      return 0;
+  }
+  " ENCHANT_API_COMPATIBLE)
+  set(CMAKE_REQUIRED_INCLUDES)
+  set(CMAKE_REQUIRED_LIBRARIES)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Enchant  DEFAULT_MSG  ENCHANT_LIBRARIES ENCHANT_INCLUDE_DIR)
+find_package_handle_standard_args(Enchant DEFAULT_MSG ENCHANT_LIBRARIES
+  ENCHANT_INCLUDE_DIR ENCHANT_API_COMPATIBLE)
 
 mark_as_advanced(ENCHANT_INCLUDE_DIR ENCHANT_LIBRARIES PC_ENCHANT_INCLUDEDIR PC_ENCHANT_LIBDIR)
--- cmake/FindLibiconv.cmake
+++ cmake/FindLibiconv.cmake
@@ -10,28 +10,28 @@
 # present in libc. Must have more robust system for release, where Gettext
 # functionality can also reside in standalone Gettext library, or the one
 # embedded within kdelibs (cf. gettext.m4 from Gettext source).
- 
+
 # Copyright (c) 2006, Chusslove Illich, <caslav.ilic@gmx.net>
 # Copyright (c) 2007, Alexander Neundorf, <neundorf@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
- 
+
 if(LIBICONV_INCLUDE_DIR AND LIBICONV_LIB_FOUND)
   set(Libiconv_FIND_QUIETLY TRUE)
 endif(LIBICONV_INCLUDE_DIR AND LIBICONV_LIB_FOUND)
- 
+
 include(CheckCCompilerFlag)
 include(CheckCSourceCompiles)
- 
+
 find_path(LIBICONV_INCLUDE_DIR iconv.h)
- 
+
 set(LIBICONV_LIB_FOUND FALSE)
- 
+
 if(LIBICONV_INCLUDE_DIR)
   include(CheckFunctionExists)
   check_function_exists(iconv_open LIBICONV_LIBC_HAS_ICONV_OPEN)
- 
+
   if (LIBICONV_LIBC_HAS_ICONV_OPEN)
     set(LIBICONV_LIBRARIES)
     set(LIBICONV_LIB_FOUND TRUE)
@@ -41,12 +41,12 @@ if(LIBICONV_INCLUDE_DIR)
       set(LIBICONV_LIB_FOUND TRUE)
     endif(LIBICONV_LIBRARIES)
   endif (LIBICONV_LIBC_HAS_ICONV_OPEN)
-  
-  if (LIBICONV_LIB_FOUND) 
+
+  if (LIBICONV_LIB_FOUND)
     check_c_compiler_flag("-Werror" ICONV_HAVE_WERROR)
     set (CMAKE_C_FLAGS_BACKUP "${CMAKE_C_FLAGS}")
     if(ICONV_HAVE_WERROR)
-        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
+      set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")
     endif(ICONV_HAVE_WERROR)
     set(CMAKE_REQUIRED_INCLUDES "${LIBICONV_INCLUDE_DIR}")
     set(CMAKE_REQUIRED_LIBRARIES "${LIBICONV_LIBRARIES}")
@@ -66,10 +66,10 @@ if(LIBICONV_INCLUDE_DIR)
     set(CMAKE_REQUIRED_LIBRARIES)
     set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_BACKUP}")
   endif(LIBICONV_LIB_FOUND)
- 
+
 endif(LIBICONV_INCLUDE_DIR)
- 
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libiconv  DEFAULT_MSG  LIBICONV_INCLUDE_DIR  LIBICONV_LIB_FOUND)
- 
+
 mark_as_advanced(LIBICONV_INCLUDE_DIR  LIBICONV_LIBRARIES  LIBICONV_LIBC_HAS_ICONV_OPEN  LIBICONV_LIB_FOUND)
