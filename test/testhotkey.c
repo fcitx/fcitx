d8839828c7a129670b080f784494d2c50a10a35f
Weng Xuetian <wengxt@gmail.com>
2012-12-08 00:13:28 -0500

    [test] add a new test case for hotkey

--- test/testhotkey.c
+++ test/testhotkey.c
@@ -17,6 +17,7 @@ int main()
     TEST_HOTKEY_UNIFICATION(FcitxKey_F, FcitxKeyState_Ctrl_Shift, FcitxKey_F, FcitxKeyState_Ctrl_Shift);
     TEST_HOTKEY_UNIFICATION(FcitxKey_F, FcitxKeyState_Ctrl, FcitxKey_F, FcitxKeyState_Ctrl);
     TEST_HOTKEY_UNIFICATION(FcitxKey_f, FcitxKeyState_Ctrl_Shift, FcitxKey_F, FcitxKeyState_Ctrl_Shift);
+    TEST_HOTKEY_UNIFICATION(FcitxKey_f, FcitxKeyState_Ctrl_Alt, FcitxKey_F, FcitxKeyState_Ctrl_Alt);
 
     return 0;
 }
