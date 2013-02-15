f9e52067a53a7423936516eb1f7899d086048ec4
Yichao Yu <yyc1992@gmail.com>
2013-02-14 22:35:52 -0500

    [skin-installer] more robust

--- src/ui/classic/fcitx-skin-installer.sh
+++ src/ui/classic/fcitx-skin-installer.sh
@@ -3,14 +3,26 @@
 export TEXTDOMAIN=fcitx
 
 if which kdialog > /dev/null 2>&1; then
-    MESSAGE_ERROR="kdialog --error"
-    MESSAGE_COMMON="kdialog --msgbox"
+    message() {
+        kdialog --msgbox "$1"
+    }
+    error() {
+        kdialog --error "$1"
+    }
 elif which zenity > /dev/null 2>&1; then
-    MESSAGE_ERROR="zenity_error"
-    MESSAGE_COMMON="zenity_info"
+    message() {
+        zenity --info --text="$1"
+    }
+    error() {
+        zenity --error --text="$1"
+    }
 else
-    MESSAGE_ERROR="echo"
-    MESSAGE_COMMON="echo"
+    message() {
+        echo "$1"
+    }
+    error() {
+        echo "$1" >&2
+    }
 fi
 
 if type gettext > /dev/null 2>&1; then
@@ -23,70 +35,44 @@ else
     }
 fi
 
-zenity_error()
-{
-    zenity --error --text="$1"
-}
-
-zenity_info()
-{
-    zenity --info --text="$1"
-}
-
-usage()
-{
-    echo "Usage: fcitx-skin-installer [skin file]"
+usage() {
+    error "Usage: $0 [skin file]"
     exit 1
-}
-
-message()
-{
-    $MESSAGE_COMMON "$1"
-}
-
-error()
-{
-    $MESSAGE_ERROR "$1"
 }
 
 if [ $# != 1 ]; then
     usage
 fi
 
-DIRCOUNT="$(tar -tf $1 | grep -c '/$')"
-if [ "x$DIRCOUNT" != "x1" ]; then
+_filelist=$(tar -tf "$1")
+
+if [ "$(echo "${_filelist}" | grep -c '^[^/]*/$' 2> /dev/null)" != "1" ]; then
     error "$(_ "Error: skin file should only contain one directory.")"
     exit 1
 fi
 
-DIRNAME="$(tar -tf $1 | grep '/$')"
-tar -tf $1 ${DIRNAME}fcitx_skin.conf >/dev/null 2>&1
-
-if [ $? != 0 ]; then
+if ! echo "${_filelist}" | grep "^[^/]*/fcitx_skin.conf" > /dev/null 2>&1; then
     error "$(_ "Error: skin file doesn't contain skin config.")"
     exit 1
 fi
 
-SKINPATH=
-
-if [ ! -z "$XDG_CONFIG_HOME" ]; then
-    SKINPATH=$XDG_CONFIG_HOME/fcitx/skin
-elif [ ! -z "$HOME" ]; then
-    SKINPATH=$HOME/.config/fcitx/skin
-fi
-
-if [ -z "$SKINPATH" ]; then
+if [ -n "$XDG_CONFIG_HOME" ]; then
+    SKINPATH="$XDG_CONFIG_HOME/fcitx/skin"
+elif [ -n "$HOME" ]; then
+    SKINPATH="$HOME/.config/fcitx/skin"
+else
     error "$(_ 'Error: $HOME or $XDG_CONFIG_HOME is not set, cannot determine the install path')"
     exit 1
 fi
 
-mkdir -p $SKINPATH || ( echo "Error: cannot create skin dir" ; exit 1 )
+mkdir -p "$SKINPATH" || {
+    echo "Error: cannot create skin dir"
+    exit 1
+}
 
-tar -C $SKINPATH -zxf $1
-
-if [ $? != 0 ]; then
+if tar -C "$SKINPATH" -xf "$1"; then
+    message "$(_ "Successfully Installed skin")"
+else
     error "$(_ "Error: skin failed to install")"
     exit 1
-else
-    message "$(_ "Successfully Installed skin")"
 fi
