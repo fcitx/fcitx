6f2fa9829fb66375778b6b7572af139c77d714c9
Yichao Yu <yyc1992@gmail.com>
2013-06-12 20:23:29 +0800

    [diagnose] typo

--- data/script/fcitx-diagnose.sh
+++ data/script/fcitx-diagnose.sh
@@ -1047,7 +1047,7 @@ check_gtk_query_immodule() {
             write_eval "$(_ 'Version Line:')"
             write_quote_str "${version_line}"
         else
-            write_eval "$(_ 'Found ${2} for unknow gtk version at ${1}.')" \
+            write_eval "$(_ 'Found ${2} for unknown gtk version at ${1}.')" \
                 "$(code_inline "${query_immodule}")" \
                 "$(code_inline gtk-query-immodules)"
             real_version=${version}
@@ -1131,7 +1131,7 @@ check_gtk_immodule_cache() {
             write_quote_str "${version_line}"
         else
             write_eval \
-                "$(_ 'Found immodule cache for unknow gtk version at ${1}.')" \
+                "$(_ 'Found immodule cache for unknown gtk version at ${1}.')" \
                 "$(code_inline "${cache}")"
             real_version=${version}
         fi
