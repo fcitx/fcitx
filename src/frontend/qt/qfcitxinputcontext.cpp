be91060f4e6f0b0499ef3efb8f00d46b57a107b0
Weng Xuetian <wengxt@gmail.com>
2020-12-08 21:12:10 -0800

    further fix for delete surrounding text

--- src/frontend/qt/qfcitxinputcontext.cpp
+++ src/frontend/qt/qfcitxinputcontext.cpp
@@ -603,7 +603,7 @@ void QFcitxInputContext::deleteSurroundingText(int offset, uint _nchar) {
 
     // validates
     if (nchar >= 0 && cursor + offset >= 0 &&
-        cursor + offset + nchar < ucsText.size()) {
+        cursor + offset + nchar <= ucsText.size()) {
         // order matters
         QVector<uint> replacedChars = ucsText.mid(cursor + offset, nchar);
         nchar = QString::fromUcs4(replacedChars.data(), replacedChars.size())
